82b69dab781e5ab95ebcd962a44799f650e868a1
Lyliun <lilya1306.dos.santos@gmail.com>
2025-11-06 15:30:23 -0300

    Commit inicial do conta_Banco

---
+++ menu.ts
@@ -0,0 +1,122 @@
+import readlinesync = require("readline-sync");
+import { colors } from "./src/Colors";
+
+/**
+ * Função principal do programa.
+ * Exibe o menu de opções e executa o loop principal que controla o sistema bancário.
+ */
+export function main() {
+
+    let opcao: number;
+
+    while (true) {
+
+        // Exibição do menu principal com estilização de cores
+        console.log(colors.bg.black, colors.fg.yellow, 
+                    "*****************************************************");
+        console.log("                                                     ");
+        console.log("                BANCO DO BRAZIL COM Z                ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Conta                          ");
+        console.log("            2 - Listar todas as Contas               ");
+        console.log("            3 - Buscar Conta por Numero              ");
+        console.log("            4 - Atualizar Dados da Conta             ");
+        console.log("            5 - Apagar Conta                         ");
+        console.log("            6 - Sacar                                ");
+        console.log("            7 - Depositar                            ");
+        console.log("            8 - Transferir valores entre Contas      ");
+        console.log("            9 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ", 
+        colors.reset);
+
+        // Solicita a opção do usuário
+        console.log("Entre com a opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        // Encerra o programa quando o usuário escolhe a opção 9
+        if (opcao == 9) {
+            console.log(colors.fg.greenstrong, 
+                "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+            sobre();
+            console.log(colors.reset, "");
+            process.exit(0);
+        }
+
+        // Estrutura de controle para direcionar cada opção
+        switch (opcao) {
+            case 1:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nCriar Conta\n\n", colors.reset);
+                keyPress();
+                break;
+            case 2:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nListar todas as Contas\n\n", colors.reset);
+                keyPress();
+                break;
+            case 3:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
+                keyPress();
+                break;
+            case 4:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nAtualizar dados da Conta\n\n", colors.reset);
+                keyPress();
+                break;
+            case 5:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nApagar uma Conta\n\n", colors.reset);
+                keyPress();
+                break;
+            case 6:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nSaque\n\n", colors.reset);
+                keyPress();
+                break;
+            case 7:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nDepósito\n\n", colors.reset);
+                keyPress();
+                break;
+            case 8:
+                console.log(colors.fg.whitestrong, 
+                    "\n\nTransferência entre Contas\n\n", colors.reset);
+                keyPress();
+                break;
+            default:
+                console.log(colors.fg.whitestrong, 
+                    "\nOpção Inválida!\n", colors.reset);
+                keyPress();
+                break;
+        }
+    }
+}
+
+/**
+ * Exibe as informações do projeto e do desenvolvedor.
+ */
+function sobre(): void {
+    console.log("\n*****************************************************");
+    console.log("Projeto Desenvolvido por: ");
+    console.log("Lilia dos Santos");
+    console.log("github.com/Lilyun/conta_Banco");
+    console.log("*****************************************************");
+}
+
+/**
+ * Pausa o programa até o usuário pressionar Enter.
+ * Usado para dar tempo de leitura entre as operações.
+ */
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
+
+// Inicia o programa
+main();
